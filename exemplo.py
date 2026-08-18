3919bb9a8efdd8796195fe2768afe216eee1f194
zarutuzuchave <rhbatwon@gmail.com>
2026-08-17 21:12:57 -0300

    aula 2

---
+++ exemplo.py
@@ -0,0 +1,20 @@
+def main():
+    val1 = int(input("Interprete o valor 1: "))
+    val2 = int(input("Interprete o valor 2: "))
+    n = somar(val1,val2)
+    print(n)
+def somar(valor,valor2=8):
+    resultado = valor + valor2
+    return resultado
+
+main()
+
+
+
+
+
+
+
+
+
+
---
+++ exemplo2.py
@@ -0,0 +1,21 @@
+#Tuplas
+#imutavel
+#igual criar uam lista só muda os colchetes para parenteses
+numeros = (1,2,3)
+#numeros = "1","2","3"
+print(numeros[0])
+print(numeros[1])
+
+
+
+tupla = ("r") #sem a virgula não identifica como tupla
+print(type(tupla))
+
+tupla1 = ("r",)#com a virgula identifica como tupla
+print(type(tupla1))
+
+
+t = tuple()#cria uma tupla vazia
+print(t)
+
+
